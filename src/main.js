84704db7ed7acbea7b5a7a259643d51709dcd7e4
wyw-s <wyw.s@foxmail.com>
2019-11-14 16:28:01 +0800

    登录模块-安装axios-配置全局使用-完成

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 引入index.less文件。
 import './styles/index.less'
+// 引入axios问价；
+import axios from 'axios'
+// 设置全局基地址；
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 把axios赋值给全局对象；
+Vue.prototype.$axios = axios
 // 注册全局 element-ui
 Vue.use(ElementUI)
 Vue.config.productionTip = false
