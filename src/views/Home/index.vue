21297f614581e57a856c1c732b93b548497ca035
wyw-s <wyw.s@foxmail.com>
2019-11-14 20:29:03 +0800

    主页模块-页面布局-完成

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-     主页
+    <!--布局开始-->
+    <el-container>
+      <!--左侧内容-->
+      <el-aside width="200px">左侧内容</el-aside>
+      <!--右侧布局容器-->
+      <el-container>
+        <el-header>顶部</el-header>
+        <el-main>
+          <!--放置二级路由容器-->
+          内容展示区
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
